5cf15a8dafa2b8757507fa44d3b47113fde24875
minwoo <cuiminyu616@gmail.com>
2022-11-26 18:59:50 +0900

    2.2
    - 3 notifications success
    5sec -> 10sec -> 15sec

--- app/src/main/java/com/example/whysoserious/MainActivity.kt
+++ app/src/main/java/com/example/whysoserious/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         val alarmManager1 = getSystemService(ALARM_SERVICE) as AlarmManager
         val alarmManager2 = getSystemService(ALARM_SERVICE) as AlarmManager
-//        val alarmManager3 = getSystemService(ALARM_SERVICE) as AlarmManager
+        val alarmManager3 = getSystemService(ALARM_SERVICE) as AlarmManager
 
         val intent1 = Intent(this,MyReceiver::class.java)
         intent1.putExtra("id",1111)
@@ -30,9 +30,9 @@ class MainActivity : AppCompatActivity() {
         intent2.putExtra("id",2222)
         intent2.putExtra("text","SECOND MESSAGE")
 
-//        val intent3 = Intent(this,MyReceiver::class.java)
-//        intent3.putExtra("id",3333)
-//        intent3.putExtra("text","THIRD MESSAGE")
+        val intent3 = Intent(this,MyReceiver::class.java)
+        intent3.putExtra("id",3333)
+        intent3.putExtra("text","THIRD MESSAGE")
 
         val pendingIntent1 = PendingIntent.getBroadcast(
             this, NOTIFICATION_ID, intent1,
@@ -46,11 +46,11 @@ class MainActivity : AppCompatActivity() {
             PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
         )
 
-//        val pendingIntent3 = PendingIntent.getBroadcast(
-//            this, 4, intent3,
-//            //PendingIntent.FLAG_NO_CREATE
-//            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-//        )
+        val pendingIntent3 = PendingIntent.getBroadcast(
+            this, 4, intent3,
+            //PendingIntent.FLAG_NO_CREATE
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
 
         val button = findViewById<ToggleButton>(R.id.button)
 
@@ -73,7 +73,7 @@ class MainActivity : AppCompatActivity() {
 
                 val triggerT1 = System.currentTimeMillis() + 5 * 1000
                 val triggerT2 = System.currentTimeMillis() + 10  * 1000
-//                val triggerT3 = System.currentTimeMillis() + 15  * 1000
+                val triggerT3 = System.currentTimeMillis() + 15  * 1000
 
                 // With setInexactRepeating(), you have to use one of the AlarmManager interval
                 // constants--in this case, AlarmManager.INTERVAL_DAY.
@@ -93,19 +93,19 @@ class MainActivity : AppCompatActivity() {
                     pendingIntent2
                 )
 
-//                alarmManager3.set(
-//                    AlarmManager.RTC_WAKEUP,
-//                    //calendar2.timeInMillis,
-//                    triggerT3,
-//                    //AlarmManager.INTERVAL_FIFTEEN_MINUTES,
-//                    pendingIntent3
-//                )
+                alarmManager3.set(
+                    AlarmManager.RTC_WAKEUP,
+                    //calendar2.timeInMillis,
+                    triggerT3,
+                    //AlarmManager.INTERVAL_FIFTEEN_MINUTES,
+                    pendingIntent3
+                )
 
                 "알림 예약이 시작되었습니다."
             } else {
                 alarmManager1.cancel(pendingIntent1)
                 alarmManager2.cancel(pendingIntent2)
-//                alarmManager3.cancel(pendingIntent3)
+                alarmManager3.cancel(pendingIntent3)
                 "알림 예약을 취소하였습니다."
             }
 
